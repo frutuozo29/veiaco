46f76cf62b3e917ecb8badda081de6568eb2d7a9
Renan Frutuozo <frutuozo29@gmail.com>
2019-03-28 20:23:23 -0300

    Test

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const server = require("./server/server");
 const dbTools = require("./tools/db_tools");
 const config = require("./config.json");
 
-var port = process.env.PORT || config.ENV.port;
-server.listen(port, () => {
-  console.log(
-    `Running on PORT: ${port} and Environment: ${process.env.NODE_ENV}`
-  );
-});
+dbTools
+  .DBConnectMongoose()
+  .then(() => {
+    var port = process.env.PORT || config.ENV.port;
+    server.listen(port, () => {
+      console.log(
+        `Running on PORT: ${port} and Environment: ${process.env.NODE_ENV}`
+      );
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
